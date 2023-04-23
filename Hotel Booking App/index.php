cdb2caeaec43079da50af09648c72b8822e415db
Willemu <108131589+Willemu@users.noreply.github.com>
2023-04-23 16:38:46 +0200

    Facilities
    Completed the facilities section

--- Hotel Booking App/index.php
+++ Hotel Booking App/index.php
@@ -295,6 +295,40 @@
   </div>
 </div>
 
+<!--Facilities -->
+<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Facilities</h2>
+
+<div class="container">
+  <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/wifi.png" width="80px">
+      <h5 class="mt-3">Wifi</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/ac.png" width="80px">
+      <h5 class="mt-3">Aircon</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/radio.png" width="80px">
+      <h5 class="mt-3">Radio</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/spa.png" width="80px">
+      <h5 class="mt-3">Spa</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/tv.png" width="80px">
+      <h5 class="mt-3">TV</h5>
+    </div>
+    <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+      <img src="images/facilities/heat.png" width="80px">
+      <h5 class="mt-3">Heater</h5>
+    </div>
+  </div>
+  <div class="col-lg-12 text-center mt-5">
+        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+    </div>  
+</div>
 
 
 <br><br><br>
