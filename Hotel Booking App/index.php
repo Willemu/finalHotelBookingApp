4a8f4c7585714f9156f1cf47f83adbc7793d0522
Willemu <108131589+Willemu@users.noreply.github.com>
2023-04-24 15:20:34 +0200

    Update index.php
    Completed the footer

--- Hotel Booking App/index.php
+++ Hotel Booking App/index.php
@@ -404,16 +404,55 @@
       <div class="col-lg-4 col-md-4 p-4 rounded mb-4">
         <div class="bg-white">
           <h5>Call Us</h5>
-          <a href="Tel: +27 111 2233" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i>+27 111 2233</a>
+          <a href="Tel: +27 111 2233" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i>+27 222 2233</a>
         </div>
+        <div class="bg-white">
+          <h5>Email Us</h5>
+          <a href="email: hot@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-envelope-fill"></i>hot@gmail.com</a>
         </div>
+      </div>
       
     </div>
   </div>
 
 </div>
 
-  
+
+
+<!--Footer -->
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+          <h3 class="h-font fw-bold fs-3 mb-2">Ultimate Hotels</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At, tempora. Debitis architecto, itaque cumque nulla omnis unde deserunt similique ut dolore minus, accusantium eum eius ducimus aperiam reiciendis sunt neque.
+          </p>
+        </div>
+        <div class="col-lg-4 p-4">
+          <h5 class="mb-3">Links</h5>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a> <br>
+          <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a> <br>
+        </div>
+        <div class="col-lg-4 p-4">
+          <h5 class="mb-5">Follow Us</h5>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+              <i class="bi bi-twitter me-1"></i> Twitter
+            </a><br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+              <i class="bi bi-facebook me-1"></i> Facebook
+            </a><br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+              <i class="bi bi-instagram me-1"></i> Instagram
+            </a><br>
+        </div>
+    </div>    
+ </div>
+
+ <h6 class="text-center bg-dark text-white p-3 m-0">Designed by WH Uys</h6> 
+
 <!-- Login Modal -->
 <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -507,17 +546,7 @@
   </div>
 </div>
 
-<!--Footer -->
- <div class="container-fluid bg-white mt-5">
-    <div class="row">
-        <div class="col-lg-4 p-4">
-          <h3 class="h-font fw-bold fs-3 mb-2">Ultimate Hotels</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At, tempora. Debitis architecto, itaque cumque nulla omnis unde deserunt similique ut dolore minus, accusantium eum eius ducimus aperiam reiciendis sunt neque.
-          </p>
-        </div>
-    </div>    
- </div>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
