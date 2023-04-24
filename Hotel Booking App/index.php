1c06e8cee68176aed05df87a9a6ce66a86a1e15c
Willemu <108131589+Willemu@users.noreply.github.com>
2023-04-24 14:15:13 +0200

    Update index.php

--- Hotel Booking App/index.php
+++ Hotel Booking App/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ultimate Hotel</title>
+    <title>Ultimate Hotels</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
@@ -37,7 +37,7 @@
 <!--Navbar and Navbar Modals -->
 <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
   <div class="container-fluid">
-    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Ultimate Hotel</a>
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Ultimate Hotels</a>
     <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -506,6 +506,18 @@
     </div>
   </div>
 </div>
+
+<!--Footer -->
+ <div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p-4">
+          <h3 class="h-font fw-bold fs-3 mb-2">Ultimate Hotels</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At, tempora. Debitis architecto, itaque cumque nulla omnis unde deserunt similique ut dolore minus, accusantium eum eius ducimus aperiam reiciendis sunt neque.
+          </p>
+        </div>
+    </div>    
+ </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
