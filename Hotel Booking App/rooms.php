b0eee5979597f03a07d957fe26e29d6599ac1a23
Willemu <108131589+Willemu@users.noreply.github.com>
2023-04-25 16:59:40 +0200

    Update rooms.php
    final updates on responsiveness

--- Hotel Booking App/rooms.php
+++ Hotel Booking App/rooms.php
@@ -172,7 +172,7 @@
               
           </div>
          </div>
-         <div class="col-md-2 text-center">
+         <div class="col-md-2 mt-lg-0 mt-md-0 mt=4 text-center">
            <h6 class="mb-4">R2000 per night</h6>
                <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
                <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
