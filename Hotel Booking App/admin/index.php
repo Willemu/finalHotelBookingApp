1c4a4cf39cd7b16e1140aa1e83793fed0dc2f948
Willemu <108131589+Willemu@users.noreply.github.com>
2023-04-28 13:35:05 +0200

    Admin Portal
    Admin portal login, pointing to SQL

--- Hotel Booking App/admin/index.php
+++ Hotel Booking App/admin/index.php
@@ -1,3 +1,8 @@
+<?php
+require("modules/essentials.php");
+require("modules/db_config.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,21 +24,42 @@
 <body class="bg-light">
 
  <div class="login-form text-center rounded bg-white shadow overflow-hidden">
-    <form>
+    <form method="POST">
         <h4 class="bg-dark text-white py-3">ADMIN LOGIN PORTAL</h4>
         <div class="p-4">
          <div class="mb-3">
-            <input name="admin_name" type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
+            <input name="admin_name" required type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
          </div>
          <div class="mb-4">
-            <input name="admin_pass" type="password" class="form-control shadow-none text-center" placeholder="Password">
+            <input name="admin_pass" required type="password" class="form-control shadow-none text-center" placeholder="Password">
          </div>
          <button name="login" type="submit" class="btn text-white custom-bg shadow-none text-center">LOGIN</button>
         </div>
     </form>
  </div>
 
+  <?php
 
+//login logic on the admin portal
+     if(isset($_POST["login"])) 
+     {
+      $frm_data = filteration($_POST);
+      $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
+      $values = [$frm_data["admin_name"],$frm_data["admin_pass"]];
+
+      $res = select($query,$values,"ss");
+      if($res->num_rows==1){
+         $row = mysqli_fetch_assoc($res);
+         session_start();
+         $_SESSION['adminLogin'] = true;
+         $_SESSION['adminId'] = $row['id'];
+      }
+      else{
+        alert('error','Login failed - Invalid Credentials!'); 
+      }
+     }
+ 
+  ?>
 
 
 <?php require("modules/script.php"); ?>
---
+++ Hotel Booking App/admin/modules/db_config.php
@@ -0,0 +1,50 @@
+<?php
+//connecting the db(hotelbooking)
+    $hname = "localhost";
+    $uname = "root";
+    $pass = "Winter01@";
+    $db = "hotelbooking";
+
+    $con = mysqli_connect($hname,$uname,$pass,$db);
+
+//message if the connection is not successful
+    if(!$con){
+        die("Cannot connect to Database".mysqli_connect_error());
+    }
+
+//parameters for whats allowed in admin login user and password blocks    
+    function filteration($data){
+        foreach($data as $key => $value){
+            $data[$key] = trim($value);
+            $data[$key] = stripcslashes($value);
+            $data[$key] = htmlspecialchars($value);
+            $data[$key] = strip_tags($value);
+        }
+        return $data;
+    }
+
+//using the splat operator ...(3dots)   
+    function select($sql,$values,$datatypes)
+    {
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con,$sql))
+        {
+            mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+            if(mysqli_stmt_execute($stmt)){
+              $res = mysqli_stmt_get_result($stmt);
+              mysqli_stmt_close($stmt);
+              return $res;
+            }
+            else{
+              mysqli_stmt_close($stmt);
+              die("Query cannot be executed - Select"); 
+            }
+        }
+            else{
+             die("Query cannot be prepared - Select");
+            }
+    }
+
+
+?> 
+
