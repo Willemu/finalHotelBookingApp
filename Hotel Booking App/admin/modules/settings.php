20df322d74025643e4563e5f4609e00172614085
Willemu <108131589+Willemu@users.noreply.github.com>
2023-05-03 13:17:39 +0200

    Settings
    Created the settings page within the admin portal

--- Hotel Booking App/admin/modules/settings.php
+++ Hotel Booking App/admin/modules/settings.php
@@ -12,7 +12,7 @@ adminLogin();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Portal - Dashboard</title>
+    <title>Admin Portal - Settings</title>
     <!--Link the Linksphp to the dashboardphp -->
     <?php require("modules/links.php"); ?>
 </head>
