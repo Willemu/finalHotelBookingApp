88972718e7f3c4dcad689fc3bc61652a2a9f4c9e
Willemu <108131589+Willemu@users.noreply.github.com>
2023-05-03 18:48:47 +0200

    trouble shoot settings page
    still trouble shooting why settings page does not want to load

--- Hotel Booking App/admin/settings.php
+++ Hotel Booking App/admin/settings.php
@@ -1,6 +1,5 @@
 <?php
 
-//link the essentialsphp to the dashboardphp 
 require("modules/essentials.php");
 adminLogin();
 ?>
@@ -13,7 +12,6 @@ adminLogin();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Portal - Dashboard</title>
-    <!--Link the Linksphp to the dashboardphp -->
     <?php require("modules/links.php"); ?>
 </head>
 
@@ -24,12 +22,11 @@ adminLogin();
     <div class="container-fluid" id="main-content">
         <div class="row">
             <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, asperiores. Consequatur, dolores distinctio placeat obcaecati eveniet aperiam officiis reiciendis id ex veritatis incidunt quaerat nulla ad at vel hic cumque?
+                Lorem ipsum 
             </div>
         </div>
     </div>
 
-<!--Link the script to the dashboardphp -->
     <?php require("modules/script.php"); ?>
 </body>
 
