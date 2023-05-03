7f6aef41d36b4200fa3ee5d6953bd267ac1d699f
Willemu <108131589+Willemu@users.noreply.github.com>
2023-05-03 10:54:09 +0200

    admin portal
    creating the admin portal menu

--- Hotel Booking App/admin/dashboard.php
+++ Hotel Booking App/admin/dashboard.php
@@ -19,75 +19,41 @@ adminLogin();
 
 <body class="bg-light">
 
-    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between">
+    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
         <h3 class="mb-0 h-font">ULTIMATE HOTELS</h3>
         <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
     </div>
 
-    <div class="col-lg-2 bg-dark border-top border-3 border-secondary">
+    <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid flex-lg-column align-items-stretch">
                 <h4 class="mt-2 text-light">ADMIN PORTAL</h4>
-                <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse flex-column align-items-stretched mt-2" id="filterDropdown">
-                    <div class="border bg-light p-3 rounded mb-3">
-                        <h5 class="mb-3" style="font: 18px;">CHECK AVAILABILITY</h5>
-                        <label class="form-label">Check-in</label>
-                        <input type="date" class="form-control shadow-none mb-3">
-                        <label class="form-label">Check-out</label>
-                        <input type="date" class="form-control shadow-none">
-                    </div>
-                </div>
-                <div class="border bg-light p-3 rounded mb-3">
-                    <h5 class="mb-3" style="font: 18px;">FACILITIES</h5>
-                    <div class="mb-2">
-                        <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
-                        <label class="form-check-label" for="f1">Aircon</label>
-                    </div>
-                    <div class="mb-2">
-                        <input type="checkbox" id="f2" class="form-check-input shadow-none me-1">
-                        <label class="form-check-label" for="f2">TV</label>
-                    </div>
-                    <div class="mb-2">
-                        <input type="checkbox" id="f3" class="form-check-input shadow-none me-1">
-                        <label class="form-check-label" for="f3">Wifi</label>
-                    </div>
-                    <div class="mb-2">
-                        <input type="checkbox" id="f4" class="form-check-input shadow-none me-1">
-                        <label class="form-check-label" for="f4">Spa</label>
-                    </div>
-                    <div class="mb-2">
-                        <input type="checkbox" id="f5" class="form-check-input shadow-none me-1">
-                        <label class="form-check-label" for="f5">Radio</label>
-                    </div>
-                    <div class="mb-2">
-                        <input type="checkbox" id="f6" class="form-check-input shadow-none me-1">
-                        <label class="form-check-label" for="f6">Heater</label>
-                    </div>
-                </div>
-                <div class="border bg-light p-3 rounded mb-3">
-                    <h5 class="mb-3" style="font: 18px;">GUESTS</h5>
-                    <div class="d-flex">
-                        <div class="me-3">
-                            <label class="form-label">Adults</label>
-                            <input type="number" class="form-control shadow-none">
-                        </div>
-                        <div>
-                            <label class="form-label">Children</label>
-                            <input type="number" class="form-control shadow-none">
-                        </div>
-                    </div>
+                <div class="collapse navbar-collapse flex-column align-items-stretched mt-2" id="adminDropdown">
+                    <ul class="nav nav-pills flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Rooms</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Users</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Settings</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-    </div>
-    </nav>
+        </nav>
 
     </div>
 
-    <!--Link the script to the dashboardphp -->
-    <?php require("modules/script.php"); ?>
+<!--Link the script to the dashboardphp -->
+    <?php require("modules/script.php.php"); ?>
 </body>
 
 </html>
